87186062d500502103a4ee4b3625c07ca8c0949b
Larissa Sant Anna <lari.santanna10@gmail.com>
2025-07-09 13:15:41 -0300

    feat: create wishList and refact the cart list

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-import items from "./services/products.js";
+import itemsService from "./services/products.js";
 import cartService from "./services/cart.js";
 
-const userCart = [];
+const itemsWhisListishList = itemsService.itemsWhisList;
+const itemsCart = itemsService.itemsCart;
 
-const item1 = items[0];
-const item2 = items[1];
-const item3 = items[2];
-const item4 = items[3];
+function breakLine() {
+    console.log('-----------------------------------');
+}
 
-const promiseItems = items.map(async (item) => {
-    return await cartService.addItemToCart(userCart, item);
-});
-await Promise.all(promiseItems);
+function wishList() {
+    if (itemsWhisListishList.length === 0) {
+        console.log('Your wish list is empty.');
+    } else {
+        itemsWhisListishList.forEach((item, index) => {
+            console.log(`${index + 1}. ${item.name} - R$${item.price}`);
+        });
+    }
+}
+
+function cart() {
+    if (itemsCart.length === 0) {
+        console.log('Your cart is empty.');
+    } else {
+        itemsCart.forEach((item, index) => {
+            console.log(`${index + 1}. ${item.name} - R$${item.price} x ${item.quantity} = R$${item.subtotal()}`);
+        });
+    }
+}
 
 console.log('Welcome to your shopping cart!');
+console.log(`\nHere are the products in your wish list:\n`);
+wishList();
+breakLine();
 console.log(`\nHere are the products in your cart:\n`);
-console.log(`1. ${item1.name} - R$${item1.price} x ${item1.quantity} = R$${item1.subtotal()}`);
-console.log(`2. ${item2.name} - R$${item2.price} x ${item2.quantity} = R$${item2.subtotal()}`);
-console.log(`3. ${item3.name} - R$${item3.price} x ${item3.quantity} = R$${item3.subtotal()}`);
-console.log(`4. ${item4.name} - R$${item4.price} x ${item4.quantity} = R$${item4.subtotal()}`);
-console.log('-----------------------------------');
-console.log(`Total: R$${(await cartService.totalValueCart(userCart))}`);
-console.log('-----------------------------------');
+cart();
+breakLine();
+console.log(`Total: R$${(await cartService.totalValueCart(itemsCart))}`);
+breakLine();
 console.log('Thank you for shopping with us!');
--- src/services/products.js
+++ src/services/products.js
@@ -7,11 +7,18 @@ async function createItem(name, price, quantity) {
     };
 }
 
-const items = [
+const itemsCart = [
     await createItem('Laptop', 1500.00, 1),
     await createItem('Shoes', 99.90, 2),
     await createItem('Shirt', 34.50, 4),
     await createItem('Hat', 19.99, 3)
 ];
 
-export default items;
+const itemsWhisList = [
+    await createItem('Smartphone', 799.99, 1),
+    await createItem('Headphones', 199.99, 1),
+    await createItem('Monitor', 299.99, 1),
+    await createItem('Keyboard', 49.99, 1)
+];
+
+export default {itemsCart, itemsWhisList};
